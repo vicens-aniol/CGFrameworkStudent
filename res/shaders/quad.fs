9766a396f8c533c522ec2392e414c98cdc692bed
stanciuantonio <antonio.stanciu01@estudiant.upf.edu>
2024-02-24 17:06:51 +0100

    Refactor shader code for exercise 2 and 3

--- res/shaders/quad.fs
+++ res/shaders/quad.fs
@@ -9,7 +9,8 @@ uniform sampler2D u_texture;
 void main()
 {
 	if (u_currentTask == 1)
-	{// SUBTASCA A DEL EJERCICIO 1
+	{
+		// SUBTASCA A DEL EJERCICIO 1
 	if (u_subtask == 1)
 	{
 		vec3 color = mix(vec3(0.0, 0.0, 1.0), vec3(1.0, 0.0, 0.0), v_uv.x);
@@ -113,46 +114,79 @@ void main()
 	} else if (u_currentTask == 2) {
 		vec4 texture_color = texture2D(u_texture, v_uv);
 		gl_FragColor = texture_color;
+
+		// SUBTASCA A DEL EJERCICIO 2
 		if (u_subtask == 1) {
-			// SUBTASCA A DEL EJERCICIO 2
-			vec3 grayscale_color = vec3(dot(texture_color.rgb, vec3(0.299, 0.587, 0.114)));
-			gl_FragColor = vec4(grayscale_color, texture_color.a);
-		}
+			// Creamos una escala de grises haciendo un producto punto entre el color de la textura y un vector con los valores estandar utilizados para la escala de grises
+		vec3 grayscale_color = vec3(dot(texture_color.rgb, vec3(0.299, 0.587, 0.114)));
+		gl_FragColor = vec4(grayscale_color, texture_color.a);
+}
 		else if (u_subtask == 2) {
-			// SUBTASCA B DEL EJERCICIO 2
-			vec3 negative_color = vec3(1.0 - texture_color.r, 1.0 - texture_color.g, 1.0 - texture_color.b);
-			gl_FragColor = vec4(negative_color, texture_color.a);
-		}
+		// SUBTASCA B DEL EJERCICIO 2
+		// Crea el negativo de la imagen restando el color de la textura a 1, invirtiendo así los colores.
+		vec3 negative_color = vec3(1.0 - texture_color.r, 1.0 - texture_color.g, 1.0 - texture_color.b);
+		gl_FragColor = vec4(negative_color, texture_color.a);
+}
 		else if (u_subtask == 3) {
-			// SUBTASCA C DEL EJERCICIO 2
-			
-		}
+		// SUBTASCA C DEL EJERCICIO 2
+		// Convierte la textura a escala de grises utilizando la luminancia y mezcla el valor de escala de grises con dos colores para obtener el efecto de duotono.
+		float grayscale = dot(texture_color.rgb, vec3(0.299, 0.587, 0.114));
+		vec3 darkColor = vec3(0.0, 0.0, 0.0); // Negro
+		vec3 lightColor = vec3(1.0, 1.0, 0.0); // Amarillo
+		vec3 duotoneColor = mix(darkColor, lightColor, grayscale);
+		gl_FragColor = vec4(duotoneColor, texture_color.a);
+}
 		else if (u_subtask == 4) {
-			// SUBTASCA D DEL EJERCICIO 2
-			vec3 color = texture_color.rgb;
-			float gray = dot(color, vec3(0.299, 0.587, 0.114));
-			float threshold = 0.5;
-			vec3 blackAndWhite = vec3(step(threshold, gray));
-			gl_FragColor = vec4(blackAndWhite, texture_color.a);
-		}
+		// SUBTASCA D DEL EJERCICIO 2
+				float gray = dot(texture_color.rgb, vec3(0.299, 0.587, 0.114));
+		float threshold = 0.5;
+		vec3 blackAndWhite = vec3(step(threshold, gray));
+		gl_FragColor = vec4(blackAndWhite, texture_color.a);
+}
 		else if (u_subtask == 5) {
-			// SUBTASCA E DEL EJERCICIO 2
-			// Calcular la distancia desde el centro de la textura
-			vec2 center = vec2(0.5, 0.5);
-			float distance = length(v_uv - center);
-
-			// Calcular el factor de oscurecimiento basado en la distancia
-			float darknessFactor = 1.0 - distance;
-
-			// Aplicar el factor de oscurecimiento al color de la textura
-			vec3 color = texture_color.rgb * darknessFactor;
-
-			// Salida del color final
-			gl_FragColor = vec4(color, texture_color.a);
+		// SUBTASCA E DEL EJERCICIO 2
+		// Oscurece el color de la textura basado en la distancia desde el centro de la textura.
+		vec2 center = vec2(0.5, 0.5);
+		float distance = length(v_uv - center);
+		float darknessFactor = 1.0 - distance;
+		vec3 color = texture_color.rgb * darknessFactor;
+		gl_FragColor = vec4(color, texture_color.a);
 		}
-		else if (u_subtask == 6) {
-			// SUBTASCA F DEL EJERCICIO 2
-			
+		else if (u_subtask == 6) 
+		{
+		// Tamaño del desenfoque
+		float blurSize = 3.0 / u_resolution.x;
+		vec4 sum = texture2D(u_texture, v_uv);
+		sum += texture2D(u_texture, v_uv + vec2(-blurSize, -blurSize));
+		sum += texture2D(u_texture, v_uv + vec2(blurSize, -blurSize));
+		sum += texture2D(u_texture, v_uv + vec2(-blurSize, blurSize));
+		sum += texture2D(u_texture, v_uv + vec2(blurSize, blurSize));
+		sum /= 5.0;
+		gl_FragColor = sum;
+		}
+	} else if (u_currentTask == 3){
+		// SUBTASCA A DEL EJERCICIO 3        
+		if (u_subtask == 1)
+		{
+			// Rota la textura 180 grados invirtiendo las coordenadas de textura
+			vec2 rotatedCoords = vec2(1.0 - v_uv.x, 1.0 - v_uv.y);
+			vec4 color = texture2D(u_texture, rotatedCoords);
+			gl_FragColor = color;
+		}
+		// SUBTASCA B DEL EJERCICIO 3	
+    	else if (u_subtask == 2) 
+		{ 
+        // Tamaño de los píxeles
+		float pixelSize = 1.0 / 50.0;
+
+		// Calcular las coordenadas del píxel correspondiente
+		vec2 pixelCoords = vec2(floor(v_uv.x / pixelSize) * pixelSize, floor(v_uv.y / pixelSize) * pixelSize);
+
+		// Obtener el color del píxel correspondiente
+		vec4 pixelColor = texture2D(u_texture, pixelCoords);
+
+		// Salida del color final
+		gl_FragColor = pixelColor;
 		}
 	}
 }
